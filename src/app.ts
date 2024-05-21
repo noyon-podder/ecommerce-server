6daab221f79774d9280b196990625a1ee792aa25
noyon-podder <noyon.podder7@gmail.com>
2024-05-22 00:05:17 +0600

    Modules file add

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express, { Response, Request, Application } from 'express'
 import cors from 'cors'
-import { UserRoutes } from './app/config/modules/user/user.route'
 
 const app: Application = express()
 
@@ -9,7 +8,9 @@ app.use(express.json())
 app.use(cors())
 
 // router
-app.use('/api/v1/student', UserRoutes)
+app.use('/api/products', (req, res) => {
+  res.send('Router perfect work')
+})
 
 // check routing
 app.get('/', (req: Request, res: Response) => {
