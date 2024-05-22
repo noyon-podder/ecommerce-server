58889aa6721657425b1bbae093707151125b7c09
noyon-podder <noyon.podder7@gmail.com>
2024-05-23 01:14:58 +0600

    all task is completed

--- src/app/modules/order/order.controller.ts
+++ src/app/modules/order/order.controller.ts
@@ -1,9 +1,45 @@
 import { Request, Response } from 'express';
 import { OrderService } from './order.service';
+import { Product } from '../product/product.model';
 
 const createOrder = async (req: Request, res: Response) => {
   try {
     const orderData = req.body;
+
+    // find the product
+    const { productId, quantity } = orderData;
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: 'Product Not Found!',
+        data: null,
+      });
+    }
+
+    // check enough quantity stock
+    if (product.inventory.quantity < quantity) {
+      return res.status(400).json({
+        success: false,
+        message: 'Insufficient Stock!!',
+      });
+    }
+
+    //reduce the product quantity
+    product.inventory.quantity -= quantity;
+    product.inventory.inStock = product.inventory.quantity > 0;
+
+    if (product.inventory.quantity <= 0) {
+      product.inventory.inStock = false;
+    } else {
+      product.inventory.inStock = true;
+    }
+
+    // Save the updated product
+    await product.save();
+
+    // create new order
     const result = await OrderService.createOrderIntoDB(orderData);
 
     //response send to client
